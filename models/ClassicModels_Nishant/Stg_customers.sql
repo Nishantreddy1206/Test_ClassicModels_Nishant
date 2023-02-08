8ba8ecd79c93a65de38b88d4952859253008a314
nishant.miriyala@bizacuity.com <nishant.miriyala@bizacuity.com>
2023-02-08 07:32:50 +0000

    Added sql codes

--- models/ClassicModels_Nishant/Stg_customers.sql
+++ models/ClassicModels_Nishant/Stg_customers.sql
@@ -1,9 +1,20 @@
 
-WITH Customer_dim as (      
+WITH Stg_payments as (
+    SELECT * FROM {{ref('Stg_payments')}}
+),
+Customer_dim as (      
     SELECT 
         customernumber as customer_id,
         salesrepemployeenumber as employee_id
     FROM public.Customers
+),
+Customer_fact as (
+    SELECT 
+        customer_id,
+        employee_id,
+        SUM(payment_amount) as payment_amount
+        FROM Customer_dim 
+        JOIN Stg_payments USING(customer_id)
+        GROUP BY 1,2
 )
-
-SELECT * FROM Customer_dim
+SELECT * FROM Customer_fact
--- models/ClassicModels_Nishant/Stg_orderdetails.sql
+++ models/ClassicModels_Nishant/Stg_orderdetails.sql
@@ -1,10 +1,22 @@
 
-WITH orderdetails_fact AS (
+WITH orders_dim as (
+    SELECT * FROM {{ref('Stg_orders')}}
+),
+orderdetails_fact AS (
     SELECT 
         ordernumber as order_id,
         SUM(quantityordered*priceeach) as order_amount
     FROM public.orderdetails
     GROUP BY 1
-)
+),
 
-SELECT * FROM orderdetails_fact
+orders_fact as (
+    SELECT
+        order_id,
+        customer_id,
+        SUM(order_amount) as order_amount
+    FROM orders_dim od
+    JOIN orderdetails_fact odf USING (order_id)
+    GROUP BY 1,2
+)
+SELECT * FROM orders_fact
